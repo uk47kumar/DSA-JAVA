69c46ce47c232c7f2faabe21e8cbd6b0dc2ade45
uk47kumar <uk47kumar@gmail.com>
2022-11-25 01:44:10 +0530

    array

---
+++ array/BuyAndSellStock.java
@@ -0,0 +1,25 @@
+package com.array;
+
+public class BuyAndSellStock {
+
+    public static int profit(int prices[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice<prices[i]){
+                int profit = prices[i]-buyPrice;
+                maxProfit = Math.max(maxProfit,profit);
+            }
+            else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(profit(prices));
+    }
+}
---
+++ array/TrappedRainWater.java
@@ -0,0 +1,33 @@
+package com.array;
+
+public class TrappedRainWater {
+
+    public static int trapped(int height[]){
+        int n = height.length;
+        // creating two auxiliary array
+
+        // first is the maximum of left
+        int leftMaximum[] = new int[n];
+        leftMaximum[0]=height[0];
+        for (int i = 1; i < n; i++) {
+            leftMaximum[i] = Math.max(height[i], leftMaximum[i-1]);
+        }
+        // and second is the maximum of right
+        int rightMaximum[] = new int[n];
+        rightMaximum[n-1]=height[n-1];
+        for (int i = n-2; i >= 0; i--) {
+            rightMaximum[i] = Math.max(height[i], rightMaximum[i+1]);
+        }
+        int trapWater = 0;
+        for (int i = 0; i < n; i++) {
+            int waterLevel = Math.min(leftMaximum[i], rightMaximum[i]);
+            trapWater += waterLevel - height[i];
+        }
+        return trapWater;
+    }
+
+    public static void main(String[] args) {
+        int height[] = {4,2,0,6,3,2,5};
+        System.out.println(trapped(height));
+    }
+}
